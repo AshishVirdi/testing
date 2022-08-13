0fbad1f1f5e5be15715140dbe859d4b272a82c91
AshishVirdi <87603531+AshishVirdi@users.noreply.github.com>
2022-08-13 16:54:29 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+// Your First Program
+// TODO Any useful task
+
+// FIXME Add new classes 
+// - HelloDad
+// - HelloMom
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
