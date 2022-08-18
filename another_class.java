5ec24ea138233bbee5947caea12b422b4f6b7b43
AshishVirdi <87603531+AshishVirdi@users.noreply.github.com>
2022-08-19 00:32:50 +0530

    Update another_class.java

--- another_class.java
+++ another_class.java
@@ -1,16 +1,16 @@
 // Java Hello World
 // changed
-// TODO task 3
+// TODO: task 3
 public class another_class {
     /* @deprecated The field num1 will be replaced by
      * NewNum field
      */
-    // TODO task 2
+    // TODO: task 2
     @Deprecated
     int num1 = 10;
     //new field
     final int NewNum = 10;
-    // TODO task 4
+    // TODO: task 4
     public static void main(String[] a){
         //create an object for the class
         another_class obj = new another_class();
