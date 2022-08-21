1ddb6c3f4bcd7047e45a1073a894c505439bf17d
AshishVirdi <87603531+AshishVirdi@users.noreply.github.com>
2022-08-22 01:27:56 +0530

    Update deprecated.java

--- deprecated.java
+++ deprecated.java
@@ -1,11 +1,18 @@
 public class deprecated {
-    /** @deprecated The function method1 will be replaced by method2
-     */
+    /**
+ * @deprecated  As of release 1.3, replaced by {@link #getPreferredSize()}
+ */
+    @Deprecated public Dimension preferredSize() {
+    return getPreferredSize();
+    }
     @Deprecated
     //old method
     public void method1(){
         System.out.println("This is a deprecated method");
     }
+    /* @deprecated The field num1 will be replaced by
+    * newnum field
+    */
     //new method
     public void method2(String m1, String m2){
         System.out.println(m1+m2);
