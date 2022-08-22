cc9d5562217997504ac77eb91c97cbd86350cf0d
AshishVirdi <87603531+AshishVirdi@users.noreply.github.com>
2022-08-23 01:19:04 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,12 +1,23 @@
-// Your First Program
-// TODO: Any useful task
-
-// FIXME Add new classes 
-// - HelloDad
-// - HelloMom
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+//DeprecatedExample.java
+class DeprecatedExample {
+ @Deprecated
+ String oldField;
+  
+ String newField;
+  
+ @Deprecated
+ public void oldMethod(){
+  System.out.println("Deprecated method invoked");
+ }
+ public void newMethod() {
+  System.out.println("Non-depricate alternative invoked");
+ }
+}
+//MainClass.java
+public class MainClass {
+ public static void main(String args[]){
+  DeprecatedExample exObj=new DeprecatedExample();
+  System.out.println(exObj.oldField);//deprecated field
+  exObj.oldMethod();//deprecated method
+ }
 }
